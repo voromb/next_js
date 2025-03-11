d7d451ffb2ff8fe3fcdd1ab6e6fe2fe97ca0842d
Voro Moran <voromb16@gmail.com>
2025-03-11 12:49:48 +0100

    Añadir todos los archivos nuevos

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+import { Invoice } from '../../lib/definitions';
+
+export default function Page() {
+    return <p>Invoices Page</p>;
+  }
---
+++ app/layout.tsx
@@ -0,0 +1,100 @@
+import '@/app/ui/global.css';
+import { montserrat } from '@/app/ui/fonts';
+ 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${montserrat.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
