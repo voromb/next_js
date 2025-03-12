751516955c3cb61090f5707e637be10d8df7aeae
Voro Moran <voromb16@gmail.com>
2025-03-12 11:21:05 +0100

    Agregado Font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,11 @@
 import { Inter } from 'next/font/google';
 import { Montserrat } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
  
 export const inter = Inter({ subsets: ['latin'] });
 export const montserrat = Montserrat({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: ['400', '700'],
+  });
